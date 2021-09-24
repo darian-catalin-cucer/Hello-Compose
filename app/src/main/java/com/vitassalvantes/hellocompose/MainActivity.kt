7ac5d808fc832e610f9b5d39ded46628f4b1cde5
VitasSalvantes <salvantesvitas@gmail.com>
2021-09-24 12:50:24 +0200

    Added theme and colors

--- app/src/main/java/com/vitassalvantes/hellocompose/MainActivity.kt
+++ app/src/main/java/com/vitassalvantes/hellocompose/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,12 +15,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.vitassalvantes.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Vitas", "Hello Compose!"))
+            HelloComposeTheme {
+                MessageCard(Message("Android", "Hello Compose!"))
+            }
         }
     }
 
@@ -29,20 +34,25 @@ class MainActivity : ComponentActivity() {
         // Add padding around our message
         Row(modifier = Modifier.padding(8.dp)) {
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                painter = painterResource(id = R.drawable.profile_picture),
                 contentDescription = "Contact profile picture",
                 modifier = Modifier
                     // Set image size to 40 dp
                     .size(40.dp)
                     // Clip image to be shaped as a circle
                     .clip(CircleShape)
+                    // Add border to image
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
 
             // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
 
             Column {
-                Text(text = msg.author)
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                )
 
                 // Add a vertical space between the author and message texts
                 Spacer(modifier = Modifier.height(4.dp))
@@ -55,6 +65,8 @@ class MainActivity : ComponentActivity() {
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(Message("Vitas", "Hello Compose!"))
+        HelloComposeTheme {
+            MessageCard(Message("Android", "Hello Compose!"))
+        }
     }
 }
