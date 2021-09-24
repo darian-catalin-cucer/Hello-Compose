a9b8bc3d5ee30c9186b446424121cd8c462f93c9
VitasSalvantes <salvantesvitas@gmail.com>
2021-09-24 13:57:09 +0200

    Added animation for displaying the messages

--- app/src/main/java/com/vitassalvantes/hellocompose/MainActivity.kt
+++ app/src/main/java/com/vitassalvantes/hellocompose/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,9 +16,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,7 +56,17 @@ class MainActivity : ComponentActivity() {
             // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
 
-            Column {
+            // We keep track if the message is expanded or not in this
+            // variable
+            var isExpanded by remember { mutableStateOf(false) }
+
+            // SurfaceColor will be updated gradually from one color to the other
+            val surfaceColor: Color by animateColorAsState(
+                if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+            )
+
+            // We toggle the isExpanded variable when we click on this Column
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
                 Text(
                     text = msg.author,
                     color = MaterialTheme.colors.secondaryVariant,
@@ -62,11 +76,23 @@ class MainActivity : ComponentActivity() {
                 // Add a vertical space between the author and message texts
                 Spacer(modifier = Modifier.height(4.dp))
 
-                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    // surfaceColor color will be changing gradually from primary to surface
+                    color = surfaceColor,
+                    // animateContentSize will change the Surface size gradually
+                    modifier = Modifier
+                        .animateContentSize()
+                        .padding(1.dp)
+                ) {
                     Text(
                         text = msg.body,
                         style = MaterialTheme.typography.body2,
-                        modifier = Modifier.padding(all = 4.dp)
+                        modifier = Modifier.padding(all = 4.dp),
+                        // If the message is expanded, we display all its content
+                        // otherwise we only display the first line
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1
                     )
                 }
             }
