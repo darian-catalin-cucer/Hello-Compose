8f8d4344ed9e8104ce253a424ae609ad31d0a73b
VitasSalvantes <salvantesvitas@gmail.com>
2021-09-24 12:29:53 +0200

    Created and decorated image and some text

--- app/src/main/java/com/vitassalvantes/hellocompose/MainActivity.kt
+++ app/src/main/java/com/vitassalvantes/hellocompose/MainActivity.kt
@@ -3,19 +3,58 @@ package com.vitassalvantes.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Compose")
+            MessageCard(Message("Vitas", "Hello Compose!"))
         }
     }
 
+    class Message(val author: String, val body: String)
+
     @Composable
-    fun MessageCard(name: String) {
-        Text(text = "Hello $name!")
+    fun MessageCard(msg: Message) {
+        // Add padding around our message
+        Row(modifier = Modifier.padding(8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+            )
+
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(text = msg.author)
+
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(text = msg.body)
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(Message("Vitas", "Hello Compose!"))
     }
 }
